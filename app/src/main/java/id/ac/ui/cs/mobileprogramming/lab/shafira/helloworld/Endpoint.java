48891d991da683004d94dfef9a6aadc675ad756a
shafiraishlah51 <shafiraish5@gmail.com>
2020-12-13 23:20:37 +0700

    Lab-7 android native

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/lab/shafira/helloworld/Endpoint.java
+++
@@ -1,5 +0,0 @@
-package id.ac.ui.cs.mobileprogramming.lab.shafira.helloworld;
-
-public class Endpoint {
-    public static final String ENDPOINT = "https://74283f78e599c8e6a5682ba224d22a3d.m.pipedream.net";
-}
